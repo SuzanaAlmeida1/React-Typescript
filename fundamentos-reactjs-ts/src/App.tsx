b9bcd104abec8d1a522329ed4295ac58320752e3
suzanaalmeida1 <zana.almeida@gmail.com>
2023-05-17 20:19:11 -0300

    página post com react e typescript

---
+++ fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,87 @@
+//extensão JSX = Java script + XML
+//Cmponente é uma função que retorna um HTML
+
+
+import {Header} from './components/Header';
+import {Post} from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+
+
+// author: {avatar_url:" ", name:" ", role: " "}
+// publishedAt: Date
+// content: String
+
+
+const posts = [
+ { id: 1,
+
+  author: {
+    avatarUrl: 'https://github.com/SuzanaAlmeida1.png',
+    name: 'Suzana Almeida',
+    role: 'Engenheira'
+  },
+   content: [
+    {type: 'paragraph', content: 'Fala galeraa 👋',},
+    {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome doprojeto é DoctorCare 🚀👉'}, 
+    {type:'link', content: 'jane.design/doctorcare'},
+        
+   ],
+
+   publishedAt: new Date('2023-04-14 20:00:00'),
+ },
+ { id: 2,
+
+  author: {
+    avatarUrl: 'https://github.com/gvergnaud.png',
+    name: 'Gabriel Vergnaud',
+    role: 'Type-level TypeScript nerd — Building'
+  },
+   content: [
+    {type: 'paragraph', content: 'Fala galeraa 👋',},
+    {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome doprojeto é DoctorCare 🚀👉'}, 
+    {type:'link', content: 'jane.design/doctorcare'},
+        
+   ],
+
+   publishedAt: new Date('2023-04-10 20:00:00'),
+ },
+];
+
+export function App() {
+ 
+
+  return (
+    
+    <div>
+
+      <Header />
+        <div className={styles.wrapper}>
+          
+        <Sidebar />
+          
+          <main>
+         {posts.map(post => {
+          return (
+          
+            <Post 
+            key={post.id}
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+          
+            />
+          )
+         })}
+         </main>
+        </div>
+    </div>
+  )
+}
+
+
+//export default App
---
+++ fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,63 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+
+export function Comment({content, onDeleteComment}: CommentProps){
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+
+    onDeleteComment(content);
+
+  }
+
+  function handleLikeComment(){
+    setLikeCount((state) => {
+      return state +1
+    });
+  }
+
+
+  return (
+    <div className={styles.comment}>
+
+    <Avatar hasBorder={false} src="https://github.com/SuzanaAlmeida1.png"/>
+
+     <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Suzana Almeida</strong>
+              <time title="06 de abril de  2023 às 21:01" dateTime="06-04-2023 21:01:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+             <p>{content}</p>
+
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+
+
+     </div>
+
+    </div>
+  )
+}
